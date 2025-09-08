88d9e5b59f7e06cd4edcbd9fcb01f9b4be1b27e7
abbm19-Abel <abbm19@gmail.com>
2025-09-08 02:01:36 -0600

    Update sum.js

--- src/sum.js
+++ src/sum.js
@@ -1,6 +1,9 @@
-export function sum(a, b) {
+function sum(a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw new TypeError('sum() expects numbers');
   }
   return a + b;
 }
+
+module.exports = { sum };
+
